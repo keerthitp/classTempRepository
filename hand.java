9eeaad4bb1cdb8589db0bb24d0e70140ce151eeb
keerthitp_git <tp.keerthana.k@gmail.com>
2019-07-08 09:31:41 -0500

    Some file to use for learning github

---
+++ hand.java
@@ -0,0 +1,30 @@
+public class Hand {
+
+	    private String state;
+
+
+	        public void setState(String state) {
+
+			        switch (state) {
+
+					            case "Grabbing":
+							                case "Waving":
+							                case "Pointing":
+							                case "Typing":
+							                    this.state = state;
+									                    break;
+											                default:
+											                    this.state = "Open";
+
+
+													            }
+
+				        System.out.println("State is set to " + this.state);
+
+					    }
+
+		    public String getState(){
+			            return this.state;
+				        }
+}
+
